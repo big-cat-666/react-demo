0e46f134851a34a833ce25ea38381a2e0939fd64
hubiao01 <hubiao01@baijia.com>
2023-11-24 14:39:12 +0800

    feat:更换目录

--- src/pages/lazyLoading/images.ts
+++
@@ -1,9 +0,0 @@
-export const images = [
-  'https://img0.baidu.com/it/u=1817709836,3411811934&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500',
-  'https://img.dahepiao.com/uploads/allimg/191010/10540H550-0.jpg',
-  'https://img0.baidu.com/it/u=1592285167,2243787823&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500',
-  'https://img1.baidu.com/it/u=717836318,1763941926&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=375',
-  'https://img2.baidu.com/it/u=2082639307,4152418384&fm=253&fmt=auto&app=120&f=JPEG?w=1422&h=800',
-  'https://img0.baidu.com/it/u=1451904453,3251967073&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500',
-  'https://img2.baidu.com/it/u=1647295341,4031695074&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500'
-];
